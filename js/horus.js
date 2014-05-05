a584d86037687825759302924fcb416857a1abe6
Alvi <alvi@alvi-LG.(none)>
2014-05-05 01:13:20 -0300

    horus

---
+++ js/horus.js
@@ -0,0 +1,100 @@
+var csvData = '1,01/07/13,Jacqueline Gomes Moreira Gurgel,1,338.94,https://avatars.githubusercontent.com/u/230542\
+\n2,01/07/13,Jacqueline Gomes Moreira Gurgel,1,338.94,https://avatars.githubusercontent.com/u/230543\
+\n3,01/07/13,Jacqueline Gomes Moreira Gurgel,1,338.94,https://avatars.githubusercontent.com/u/230544\
+\n4,01/07/13,Jacqueline Gomes Moreira Gurgel,1,338.94,https://avatars.githubusercontent.com/u/230545\
+\n5,01/07/13,Jacqueline Gomes Moreira Gurgel,1,338.94,https://avatars.githubusercontent.com/u/230546\
+\n6,01/07/13,Jacqueline Gomes Moreira Gurgel,1,338.94,https://avatars.githubusercontent.com/u/230547\
+\n7,01/07/13,Jacqueline Gomes Moreira Gurgel,1,338.94,https://avatars.githubusercontent.com/u/230548\
+\n8,01/07/13,Jacqueline Gomes Moreira Gurgel,1,338.94,https://avatars.githubusercontent.com/u/230549\
+\n9,01/07/13,Gustavo Gomes Peixoto,6,136.91,https://avatars.githubusercontent.com/u/230550\
+\n10,01/07/13,Gustavo Gomes Peixoto,6,136.91,https://avatars.githubusercontent.com/u/230551\
+\n11,01/07/13,Gustavo Gomes Peixoto,6,136.91,https://avatars.githubusercontent.com/u/230552\
+\n12,01/08/13,Gustavo Gomes Peixoto,6,136.91,https://avatars.githubusercontent.com/u/230553\
+\n13,01/08/13,Gustavo Gomes Peixoto,6,136.91,https://avatars.githubusercontent.com/u/230554\
+\n14,01/08/13,Gustavo Gomes Peixoto,6,136.91,https://avatars.githubusercontent.com/u/230555\
+\n15,01/08/13,Gustavo Gomes Peixoto,6,136.91,https://avatars.githubusercontent.com/u/230556\
+\n16,01/08/13,Gustavo Gomes Peixoto,6,136.91,https://avatars.githubusercontent.com/u/230557\
+\n17,01/08/13,Gustavo Gomes Peixoto,6,136.91,https://avatars.githubusercontent.com/u/230558\
+\n18,01/08/13,Alberto Marcos de Andrade,5,021.11,https://avatars.githubusercontent.com/u/230559\
+\n19,01/08/13,Alberto Marcos de Andrade,5,021.11,https://avatars.githubusercontent.com/u/230560\
+\n20,01/08/13,Alberto Marcos de Andrade,5,021.11,https://avatars.githubusercontent.com/u/230561\
+\n21,01/08/13,Alberto Marcos de Andrade,5,021.11,https://avatars.githubusercontent.com/u/230562\
+\n22,01/08/13,Alberto Marcos de Andrade,5,021.11,https://avatars.githubusercontent.com/u/230563\
+\n23,01/08/13,Alberto Marcos de Andrade,5,021.11,https://avatars.githubusercontent.com/u/230564\
+\n24,01/09/13,Alberto Marcos de Andrade,5,021.11,https://avatars.githubusercontent.com/u/230565\
+\n25,01/09/13,Alberto Marcos de Andrade,5,021.11,https://avatars.githubusercontent.com/u/230566\
+\n26,01/09/13,Alberto Marcos de Andrade,5,021.11,https://avatars.githubusercontent.com/u/230567\
+\n27,01/09/13,Alberto Marcos de Andrade,5,021.11,https://avatars.githubusercontent.com/u/230568\
+\n28,01/09/13,Paulo Soares Pinto,6,136.91,https://avatars.githubusercontent.com/u/230569\
+\n29,01/09/13,Paulo Soares Pinto,6,136.91,https://avatars.githubusercontent.com/u/230570\
+\n30,01/09/13,Paulo Soares Pinto,6,136.91,https://avatars.githubusercontent.com/u/230571\
+\n31,01/09/13,Paulo Soares Pinto,6,136.91,https://avatars.githubusercontent.com/u/230572\
+\n32,01/09/13,Paulo Soares Pinto,6,136.91,https://avatars.githubusercontent.com/u/230573\
+\n33,01/09/13,Paulo Soares Pinto,6,136.91,https://avatars.githubusercontent.com/u/230574\
+\n34,01/10/13,Paulo Soares Pinto,6,136.91,https://avatars.githubusercontent.com/u/230575\
+\n35,01/10/13,Paulo Soares Pinto,6,136.91,https://avatars.githubusercontent.com/u/230576\
+\n36,01/10/13,Paulo Soares Pinto,6,136.91,https://avatars.githubusercontent.com/u/230577\
+\n37,01/10/13,Stefane dos Santos Oliveira Pereira,3,347.4,https://avatars.githubusercontent.com/u/230578\
+\n38,01/10/13,Stefane dos Santos Oliveira Pereira,3,347.4,https://avatars.githubusercontent.com/u/230579\
+\n39,01/10/13,Wilson de Oliveira,3,347.4,https://avatars.githubusercontent.com/u/230580\
+\n40,01/10/13,Wilson de Oliveira,3,347.4,https://avatars.githubusercontent.com/u/230581\
+\n41,01/10/13,Wilson de Oliveira,3,347.4,https://avatars.githubusercontent.com/u/230582\
+\n42,01/10/13,Wilson de Oliveira,3,347.4,https://avatars.githubusercontent.com/u/230583\
+\n43,01/10/13,Wilson de Oliveira,3,347.4,https://avatars.githubusercontent.com/u/230584\
+\n44,01/10/13,Wilson de Oliveira,3,347.4,https://avatars.githubusercontent.com/u/230585\
+\n45,01/11/13,Wilson de Oliveira,3,347.4,https://avatars.githubusercontent.com/u/230586\
+\n46,01/11/13,Wilson de Oliveira,3,347.4,https://avatars.githubusercontent.com/u/230587\
+\n47,01/11/13,Wilson de Oliveira,3,347.4,https://avatars.githubusercontent.com/u/230588\
+\n48,01/11/13,Wilson de Oliveira,3,347.4,https://avatars.githubusercontent.com/u/230589\
+\n49,01/11/13,Sila Mirelle Gomes Ferreira,3,347.4,https://avatars.githubusercontent.com/u/230590\
+\n50,01/11/13,Sila Mirelle Gomes Ferreira,3,347.4,https://avatars.githubusercontent.com/u/230591\
+\n51,01/11/13,Sila Mirelle Gomes Ferreira,3,347.4,https://avatars.githubusercontent.com/u/230592\
+\n52,01/11/13,Sila Mirelle Gomes Ferreira,3,347.4,https://avatars.githubusercontent.com/u/230593\
+\n53,01/11/13,Sila Mirelle Gomes Ferreira,3,347.4,https://avatars.githubusercontent.com/u/230594\
+\n54,01/11/13,Sila Mirelle Gomes Ferreira,3,347.4,https://avatars.githubusercontent.com/u/230595\
+\n55,01/12/13,Sila Mirelle Gomes Ferreira,3,347.4,https://avatars.githubusercontent.com/u/230596\
+\n56,01/12/13,Sila Mirelle Gomes Ferreira,3,347.4,https://avatars.githubusercontent.com/u/230597\
+\n57,01/12/13,Sila Mirelle Gomes Ferreira,3,347.4,https://avatars.githubusercontent.com/u/230598\
+\n58,01/12/13,Sila Mirelle Gomes Ferreira,3,347.4,https://avatars.githubusercontent.com/u/230599\
+\n59,01/12/13,Alice de Oliveira Garcia,5,021.11,https://avatars.githubusercontent.com/u/230600\
+\n60,01/12/13,Alice de Oliveira Garcia,5,021.11,https://avatars.githubusercontent.com/u/230601\
+\n61,01/12/13,Alice de Oliveira Garcia,5,021.11,https://avatars.githubusercontent.com/u/230602\
+\n62,01/12/13,Alice de Oliveira Garcia,5,021.11,https://avatars.githubusercontent.com/u/230603\
+\n63,01/12/13,Alice de Oliveira Garcia,5,021.11,https://avatars.githubusercontent.com/u/230604\
+\n64,01/12/13,Alice de Oliveira Garcia,5,021.11,https://avatars.githubusercontent.com/u/230605\
+\n65,01/01/14,Alice de Oliveira Garcia,5,021.11,https://avatars.githubusercontent.com/u/230606\
+\n66,01/01/14,Alice de Oliveira Garcia,5,021.11,https://avatars.githubusercontent.com/u/230607\
+\n67,01/01/14,Alice de Oliveira Garcia,5,021.11,https://avatars.githubusercontent.com/u/230608\
+\n68,01/01/14,Atilio Antonio Belote,5,021.11,https://avatars.githubusercontent.com/u/230609\
+\n69,01/01/14,Atilio Antonio Belote,5,021.11,https://avatars.githubusercontent.com/u/230610\
+\n70,01/01/14,Atilio Antonio Belote,5,021.11,https://avatars.githubusercontent.com/u/230611\
+\n71,01/01/14,Atilio Antonio Belote,5,021.11,https://avatars.githubusercontent.com/u/230612\
+\n72,01/01/14,Atilio Antonio Belote,5,021.11,https://avatars.githubusercontent.com/u/230613\
+\n73,01/01/14,Atilio Antonio Belote,5,021.11,https://avatars.githubusercontent.com/u/230614\
+\n74,01/01/14,Atilio Antonio Belote,5,021.11,https://avatars.githubusercontent.com/u/230615\
+\n75,01/02/14,Atilio Antonio Belote,5,021.11,https://avatars.githubusercontent.com/u/230616\
+\n76,01/02/14,Atilio Antonio Belote,5,021.11,https://avatars.githubusercontent.com/u/230617\
+\n77,01/02/14,Andreia Passos Santana Winkler,3,347.4,https://avatars.githubusercontent.com/u/230618\
+\n231,01/02/14,Gustavo Gomes Peixoto,771.26,https://avatars.githubusercontent.com/u/230619\
+\n78,01/02/14,Andreia Passos Santana Winkler,3,347.4,https://avatars.githubusercontent.com/u/230620\
+\n79,01/02/14,Andreia Passos Santana Winkler,3,347.4,https://avatars.githubusercontent.com/u/230621\
+\n80,01/02/14,Andreia Passos Santana Winkler,3,347.4,https://avatars.githubusercontent.com/u/230622\
+\n81,01/03/14,Andreia Passos Santana Winkler,3,347.4,https://avatars.githubusercontent.com/u/230623\
+\n82,01/03/14,Andreia Passos Santana Winkler,3,347.4,https://avatars.githubusercontent.com/u/230624\
+\n83,01/03/14,Andreia Passos Santana Winkler,3,347.4,https://avatars.githubusercontent.com/u/230625\
+\n84,01/03/14,Andreia Passos Santana Winkler,3,347.4,https://avatars.githubusercontent.com/u/230626\
+\n85,01/03/14,Andreia Passos Santana Winkler,3,347.4,https://avatars.githubusercontent.com/u/230627\
+\n86,01/03/14,Andreia Passos Santana Winkler,3,347.4,https://avatars.githubusercontent.com/u/230628\
+\n87,01/03/14,Jacqueline Gomes Moreira Gurgel,1,338.94,https://avatars.githubusercontent.com/u/230629\
+\n88,01/03/14,Igor Daniel Policarpo,678,https://avatars.githubusercontent.com/u/230630\
+\n89,01/03/14,Igor Daniel Policarpo,678,https://avatars.githubusercontent.com/u/230631\
+\n90,01/03/14,Igor Daniel Policarpo,678,https://avatars.githubusercontent.com/u/230632\
+\n91,01/04/14,Karla Simões Araujo,3,347.4,https://avatars.githubusercontent.com/u/230633\
+\n92,01/04/14,Karla Simoes Araujo,3,347.4,https://avatars.githubusercontent.com/u/230634\
+\n93,01/04/14,Karla Simoes Araujo,3,347.4,https://avatars.githubusercontent.com/u/230635\
+\n94,01/04/14,Karla Simoes Araujo,3,347.4,https://avatars.githubusercontent.com/u/230636\
+\n95,01/04/14,Karla Simoes Araujo,3,347.4,https://avatars.githubusercontent.com/u/230637\
+\n96,01/04/14,Karla Simoes Araujo,3,347.4,https://avatars.githubusercontent.com/u/230638\
+\n97,01/04/14,Karla Simoes Araujo,3,347.4,https://avatars.githubusercontent.com/u/230639\
+\n98,01/04/14,Alberto Marcos de Andrade,5,021.11,https://avatars.githubusercontent.com/u/230640\
+\n99,01/04/14,Alice de Oliveira Garcia,1,115.8,https://avatars.githubusercontent.com/u/230641';
